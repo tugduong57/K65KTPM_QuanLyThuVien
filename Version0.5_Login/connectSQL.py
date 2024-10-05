96134ee8670947ace95bca0cdc54919c4ca2a685
Ngoc Luong <tranngocluong2k5@gmai.com>
2024-10-05 10:14:17 +0700

    use database for sign up sign in

---
+++ Version0.5_Login/connectSQL.py
@@ -0,0 +1,265 @@
+# First: Create/Connect database 
+import pyodbc
+
+def C_database():
+	conn_string = (
+	    "DRIVER={ODBC Driver 17 for SQL Server};"
+	    r"SERVER=localhost\SQLEXPRESS;"  		# hoặc tên máy chủ 
+	    "DATABASE=master;"  
+	    "Trusted_Connection=yes;"  	# Sử dụng Windows Authentication
+	)
+
+	conn = pyodbc.connect(conn_string, autocommit=True)	# Kết nối đến SQL Server
+	cursor = conn.cursor()								# Tạo con trỏ để thực hiện các câu lệnh SQL
+
+	database_name = 'Quan_Ly_Thu_Vien'			# Tạo database
+
+	create_db = f''' 
+		IF NOT EXISTS (
+	    	SELECT name 
+	   		FROM sys.databases 
+	   		WHERE name = '{database_name}'
+		)
+		BEGIN 
+	   		CREATE DATABASE {database_name}; 
+		END
+	'''
+
+	cursor.execute(create_db)
+
+	# Sử dụng cơ sở dữ liệu Quan_Ly_Thu_Vien
+	use_db = f'USE {database_name}'
+	cursor.execute(use_db)
+
+	# Tạo bảng admin
+	create_admin = '''
+		if not exists ( select * from sysobjects where name = 'admin' and xtype = 'U')
+		begin
+			create table admin (
+				tai_khoan varchar(20) not null primary key,
+				mat_khau varchar(20) not null
+			)
+		end
+	'''
+
+	cursor.execute(create_admin)
+
+	# Tạo bảng sách
+	create_sach = '''
+		if not exists ( select * from sysobjects where name = 'sach' and xtype = 'U')
+		begin
+			create table sach (
+				ma_sach char(20) not null primary key,
+				ten_sach nchar(30),
+				the_loai nchar(20),
+				ngay_xuat_ban date,
+				nha_xuat_ban nchar(30),
+				tac_gia nchar(30),
+				gia_sach int,
+				tong_so_luong int
+			)
+		end
+	'''
+
+	cursor.execute(create_sach)
+
+	# Tạo bảng độc giả
+	create_doc_gia = '''
+		if not exists ( select * from sysobjects where name = 'doc_gia' and xtype = 'U')
+		begin
+			create table doc_gia (
+				ma_doc_gia char(20) not null primary key,
+				ho_ten nchar(30),
+				so_DT varchar(15),
+				trang_thai nchar(30)
+			)
+		end
+	'''
+
+	cursor.execute(create_doc_gia)
+
+	# Tạo bảng phiếu mượn
+	create_phieu_muon = '''
+		if not exists ( select * from sysobjects where name = 'phieu_muon' and xtype = 'U')
+		begin
+			create table phieu_muon (
+				ma_phieu char(20) not null primary key,
+				ma_doc_gia char(20),
+				ngay_muon date,
+				han_tra_sach date,
+				ma_sach char(20),
+				foreign key (ma_sach) references sach(ma_sach),
+				foreign key (ma_doc_gia) references doc_gia(ma_doc_gia)
+			)
+		end
+	'''
+
+	cursor.execute(create_phieu_muon)
+
+	# Tạo bảng danh sách mượn
+	create_DS_muon = '''
+		if not exists ( select * from sysobjects where name = 'danh_sach_muon' and xtype = 'U')
+		begin
+			create table danh_sach_muon (
+				ma_phieu char(20) not null,
+				ma_sach char(20) not null,
+				primary key (ma_phieu, ma_sach),
+				foreign key (ma_phieu) references phieu_muon(ma_phieu)
+			)
+		end
+	'''
+
+	cursor.execute(create_DS_muon)
+
+	# Đóng kết nối
+	cursor.close()
+	conn.close()
+
+	print("Kết nối thành công !")
+
+
+# Check đăng nhập
+def check_sign_in(username, password_input) :
+
+	conn_string = (
+        "DRIVER={ODBC Driver 17 for SQL Server};"
+        r"SERVER=localhost\SQLEXPRESS;"
+        "DATABASE=Quan_Ly_Thu_Vien;"  # Kết nối với database đã tạo
+        "Trusted_Connection=yes;"
+    )
+	conn = pyodbc.connect(conn_string, autocommit=True)	# Kết nối đến SQL Server
+	cursor = conn.cursor()				# Tạo con trỏ để thực hiện các câu lệnh SQL
+
+	query = f"SELECT tai_khoan, mat_khau FROM admin WHERE tai_khoan = '{username}' AND mat_khau = '{password_input}'"
+	cursor.execute(query)
+	res = cursor.fetchone()
+	if res :
+		return True
+	else :
+		return False
+
+
+	# Đóng kết nối
+	cursor.close()
+	conn.close()
+
+
+
+# Thêm giá trị vào bảng Admin
+def Insert_admin(tai_khoan, mat_khau) :
+	conn_string = (
+        "DRIVER={ODBC Driver 17 for SQL Server};"
+        r"SERVER=localhost\SQLEXPRESS;"
+        "DATABASE=Quan_Ly_Thu_Vien;"  # Kết nối với database đã tạo
+        "Trusted_Connection=yes;"
+    )
+
+	conn = pyodbc.connect(conn_string, autocommit=True)	# Kết nối đến SQL Server
+	cursor = conn.cursor()				# Tạo con trỏ để thực hiện các câu lệnh SQL
+
+	insert_values = f'''
+		insert into admin (tai_khoan, mat_khau)
+		values ('{tai_khoan}', '{mat_khau}');
+	'''
+	cursor.execute(insert_values)
+
+	# Đóng kết nối
+	cursor.close()
+	conn.close()
+	print(f"Đã chèn tài khoản {tai_khoan} thành công!")
+
+
+
+# Thêm giá trị vào bảng sach
+def Insert_bang_sach(ma_sach, ten_sach, the_loai, ngay_xuat_ban, nha_xuat_ban, tac_gia, gia_sach, tong_so_luong) :
+	conn_string = (
+        "DRIVER={ODBC Driver 17 for SQL Server};"
+        r"SERVER=localhost\SQLEXPRESS;"
+        "DATABASE=Quan_Ly_Thu_Vien;"  # Kết nối với database đã tạo
+        "Trusted_Connection=yes;"
+    )
+
+	conn = pyodbc.connect(conn_string, autocommit=True)	# Kết nối đến SQL Server
+	cursor = conn.cursor()								# Tạo con trỏ để thực hiện các câu lệnh SQL
+
+	insert_values = f'''
+		insert into sach
+		values ('{ma_sach}', N'{ten_sach}', N'{the_loai}', '{ngay_xuat_ban}', N'{nha_xuat_ban}', N'{tac_gia}', '{gia_sach}', '{tong_so_luong}');
+	'''
+
+	cursor.execute(insert_values)
+	# Đóng kết nối
+	cursor.close()
+	conn.close()
+
+
+
+# Thêm giá trị vào bảng độc giả
+def Insert_doc_gia(ma_doc_gia, ho_ten, so_DT, trang_thai) :
+	conn_string = (
+        "DRIVER={ODBC Driver 17 for SQL Server};"
+        r"SERVER=localhost\SQLEXPRESS;"
+        "DATABASE=Quan_Ly_Thu_Vien;"  # Kết nối với database đã tạo
+        "Trusted_Connection=yes;"
+    )
+
+	conn = pyodbc.connect(conn_string, autocommit=True)	# Kết nối đến SQL Server
+	cursor = conn.cursor()								# Tạo con trỏ để thực hiện các câu lệnh SQL
+
+	insert_values = f'''
+		insert into doc_gia
+		values ('{ma_doc_gia}', N'{ho_ten}', '{so_DT}', N'{trang_thai}');
+	'''
+	cursor.execute(insert_values)
+
+	# Đóng kết nối
+	cursor.close()
+	conn.close()
+
+
+
+# Thêm giá trị vào bảng phiếu mượn
+def Insert_phieu_muon(ma_phieu, ma_doc_gia, ngay_muon, han_tra_sach, ma_sach) :
+	conn_string = (
+        "DRIVER={ODBC Driver 17 for SQL Server};"
+        r"SERVER=localhost\SQLEXPRESS;"
+        "DATABASE=Quan_Ly_Thu_Vien;"  # Kết nối với database đã tạo
+        "Trusted_Connection=yes;"
+    )
+
+	conn = pyodbc.connect(conn_string, autocommit=True)	# Kết nối đến SQL Server
+	cursor = conn.cursor()								# Tạo con trỏ để thực hiện các câu lệnh SQL
+
+	insert_values = f'''
+		insert into phieu_muon
+		values ('{ma_phieu}', '{ma_doc_gia}', '{ngay_muon}', '{han_tra_sach}', '{ma_sach}');
+	'''
+	cursor.execute(insert_values)
+
+	# Đóng kết nối
+	cursor.close()
+	conn.close()
+
+
+
+# Thêm giá trị vào bảng danh sách mượn
+def Insert_danh_sach_muon(ma_phieu, ma_sach) :
+	conn_string = (
+        "DRIVER={ODBC Driver 17 for SQL Server};"
+        r"SERVER=localhost\SQLEXPRESS;"
+        "DATABASE=Quan_Ly_Thu_Vien;"  # Kết nối với database đã tạo
+        "Trusted_Connection=yes;"
+    )
+
+	conn = pyodbc.connect(conn_string, autocommit=True)	# Kết nối đến SQL Server
+	cursor = conn.cursor()								# Tạo con trỏ để thực hiện các câu lệnh SQL
+
+	insert_values = f'''
+		insert into danh_sach_muon
+		values ('{ma_phieu}', '{ma_sach}');
+	'''
+	cursor.execute(insert_values)
+
+	# Đóng kết nối
+	cursor.close()
+	conn.close()
---
+++ Version0.5_Login/sign_in.py
@@ -0,0 +1,126 @@
+from tkinter import *
+from tkinter import messagebox
+import os
+import Version0_5
+from PIL import Image, ImageTk 
+import sign_up
+import connectSQL
+
+def sign_in(root):
+
+    def mouse_index(event):
+        x, y = event.x, event.y
+        print(f"Vị trí chuột: x={x}, y={y}")
+    root.bind("<Button-1>", mouse_index)
+
+    def on_label_click(event):
+        event.widget.focus_set()
+        print("Label was clicked!")
+
+    def show_frame(frame):
+        frame.tkraise()
+
+    PathOfFile = os.path.abspath(__file__)
+    PathOfFile = os.path.dirname(PathOfFile).replace("\\", "/")
+
+    window_width = 1050; window_height = 570
+
+    def Xuly_Anh(imageOfpath, size_width, size_height):
+        original_image = Image.open(imageOfpath)
+        image = original_image.resize((size_width, size_height), resample=Image.LANCZOS)
+        image = ImageTk.PhotoImage(image)
+        return image
+
+    # Tạo frame đăng nhập
+    frame_sign_in = Frame(root)
+    frame_sign_in.grid(row=0, column=0, sticky="nsew")
+
+    show_frame(frame_sign_in)
+
+    # Thêm hình nền
+    img_bgLogIn = Xuly_Anh(PathOfFile + "/Image/" + 'bgLogIn.png', window_width, window_height)
+    label_LogIn = Label(frame_sign_in, image = img_bgLogIn, borderwidth = 0, highlightthickness = 0); label_LogIn.place(x = 0, y = 0)
+    label_LogIn.bind("<Button-1>", on_label_click)
+    
+    # Check đăng nhập
+    def login_user():
+        username = user.get()
+        password_input = password.get()
+
+        check = connectSQL.check_sign_in(username, password_input)
+        if check : 
+            print("Login successful")
+            Version0_5.quanlykhosach(root)
+            return
+
+        messagebox.showerror("Error", "Invalid username or password")
+
+        # # Đọc database
+        # with open(PathOfFile + '/bg_icon/users.txt', 'r') as file:
+        #     users = file.readlines()
+        #     for line in users:
+        #         stored_user, stored_password = line.strip().split(',')
+        #         print(f"User: {stored_user}, Password: {stored_password}")
+        #         if stored_user == username and stored_password == password_input:
+        #             print("Login successful")
+        #             Version0_5.quanlykhosach(root)
+        #             return
+        #     messagebox.showerror("Error", "Invalid username or password")
+
+    def signup_user():
+        sign_up.sign_up(root);
+        return
+
+    ## Các hàm để tạo Dòng chữ mờ tại Entry  - placeholder
+    def set_placeholder(entry, placeholder_text):
+        entry.insert(0, placeholder_text); entry.config(fg='grey')
+
+    def on_entry_click(event, entry, placeholder_text):
+        if entry.get() == placeholder_text:
+            entry.delete(0, "end"); entry.config(fg='#145da0')
+
+    def on_focusout(event, entry, placeholder_text):
+        if entry.get() == "":
+            entry.insert(0, placeholder_text); entry.config(fg='grey')
+
+    def apply_placeholder(entry, placeholder_text):
+        # Áp dụng placeholder cho một Entry.
+        set_placeholder(entry, placeholder_text) 
+        entry.bind("<FocusIn>", lambda event: on_entry_click(event, entry, placeholder_text))
+        entry.bind("<FocusOut>", lambda event: on_focusout(event, entry, placeholder_text))
+
+    # Nhập tên người dùng
+    user = Entry(frame_sign_in, justify='center', width=19, bg = 'white', fg = '#145da0', font =("Poppins", 17), border=0)
+    user.place(x=383, y=198)
+    apply_placeholder(user, "Tài khoản")
+
+    # Nhập mật khẩu
+    password = Entry(frame_sign_in, justify='center', width = 19, bg = 'white', fg = '#145da0', font =("Poppins", 17), border=0, show = '*')
+    password.place(x = 383, y=263)
+    apply_placeholder(password, "************")
+
+    user.bind('<Return>', lambda _: login_user())
+    password.bind('<Return>', lambda _: login_user())
+
+    def on_enter(event):
+        event.widget.config(cursor='hand2')     # Thay đổi kiểu con trỏ khi chuột vào label
+
+    def on_leave(event):
+        event.widget.config(cursor='')          # Khôi phụckiểu con trỏ khi chuột ra khỏi label
+
+    # Nút đăng nhập
+    login = Button(frame_sign_in, text='Đăng nhập', width=15, bg = '#145da0', fg = 'white', 
+                    font =("Poppins", 15), border=0, command=lambda: login_user())
+    login.place(x=420, y=315)
+
+    # Nút đăng kí
+    signup = Button(frame_sign_in, text='Đăng ký', width=15, bg = '#145da0', fg = 'white', 
+                    font =("Poppins", 15), border=0, command=lambda: signup_user())
+    signup.place(x=420, y=366)
+
+    for B in [login, signup]:
+        B.bind("<Enter>", on_enter)
+        B.bind("<Leave>", on_leave)
+
+    root.update_idletasks()
+    root.mainloop()
